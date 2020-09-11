1835c8b37ed907a658895bf9f35c0d6d43e85afa
Websoft Technology Nepal <websoft.pokhara@gmail.com>
2020-09-11 10:37:35 +0545

    design start

---
+++ index.php
@@ -0,0 +1,147 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="fontawesome/css/all.css">
+	<link rel="stylesheet" href="css/style.css">
+	<body>
+		<section id="topheader">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 left-topheader">
+						Welcome to our shop
+					</div>
+					<div class="col-md-8 right-topheader">
+						<i class="fab fa-facebook-f"></i>
+						<i class="fab fa-instagram-square"></i>
+						<i class="fab fa-twitter-square"></i>
+						<i class="fab fa-youtube-square"></i>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<img src="img/logo.png" alt="">
+					</div>
+					<div class="col-md-6 searchbox">
+						<input type="text" name="search" placeholder="Type to search here ....">
+						<button type="submit">
+							<i class="fas fa-search"></i>
+						</button>
+					</div>
+					<div class="col-md-3 logininfo">
+						<i class="fa fa-user-alt"></i>
+						<span class="logininfo-content">
+							<span><strong>My Account</strong></span>
+							<span>Register & Login</span>
+						</span>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<span style="color: #fff">TOP CATAGORIES</span>
+					</div>
+					<div class="col-md-9">
+						
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="slider">
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+	            <div class="carousel-inner">
+				    <div class="carousel-item active">
+				      <img src="img/slider/1.jpg" class="d-block w-100" alt="...">
+				    </div>
+				    <div class="carousel-item">
+				      <img src="img/slider/2.jpg" class="d-block w-100" alt="...">
+				    </div>
+				    <div class="carousel-item">
+				      <img src="img/slider/3.jpg" class="d-block w-100" alt="...">
+				    </div>
+	  			</div>
+			  	<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    	<span class="sr-only">Previous</span>
+			  	</a>
+			  	<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    	<span class="sr-only">Next</span>
+			  	</a>
+			</div>
+		</section>
+		<section id="bannerarea">
+			
+			<div class="row">
+				<div class="slider_banner">
+		          <div class="slide-banner-info col-md-6" style="float: left;">
+		            <div class="banner-col">
+		              <a href="#"><img src="https://kulimpoint.websofttechnology.com.my/site/img/banner1.jpg" alt="" class="img-responsive" width="auto" height="auto"></a>
+		            </div>
+		          </div>
+		          <div class="slide-banner-info col-md-6" style="float: right;">
+		            <div class="banner-col">
+		              <a href="#"><img src="https://kulimpoint.websofttechnology.com.my/site/img/banner2.jpg" alt="" class="img-responsive" width="auto" height="auto"></a>
+		            </div>
+		          </div>
+          			<div class="clearfix"></div>
+        		</div>
+        	
+        	</div>
+		</section>
+		<section id="catagoryarea">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 titlebox">
+						TOP CATAGORIES
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+						<img src="img/catagory1.jpg" class="img-thumbnail" alt="">
+					</div>
+					<div class="col-md-3">
+						<img src="img/catagory1.jpg" class="img-thumbnail" alt="">
+					</div>
+					<div class="col-md-3">
+						<img src="img/catagory1.jpg" class="img-thumbnail" alt="">
+					</div>
+					<div class="col-md-3">
+						<img src="img/catagory1.jpg" class="img-thumbnail" alt="">
+					</div>
+					<div class="col-md-3">
+						<img src="img/catagory1.jpg" class="img-thumbnail" alt="">
+					</div>
+					<div class="col-md-3">
+						<img src="img/catagory1.jpg" class="img-thumbnail" alt="">
+					</div>
+					<div class="col-md-3">
+						<img src="img/catagory1.jpg" class="img-thumbnail" alt="">
+					</div>
+					<div class="col-md-3">
+						<img src="img/catagory1.jpg" class="img-thumbnail" alt="">
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+
+
+
+
+
+	<script src="js/jquery.js"></script>
+	<script src="fontawesome/js/all.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+	</body>
+</html>
